5beab3d507d2ebec9a42ede8c60e946be75b2644
kerols-Soliman <ksoliman998@gmail.com>
2021-05-09 01:31:24 +0200

    add service

---
+++ src/app/interface/Product.ts
@@ -0,0 +1,10 @@
+export interface IProduct{
+        "Id":number,
+        "Name":string,
+        "Price":number,
+        "Quentity":number,
+        "Description":string,
+        "Image":string,
+        "Discount":number ,
+        "Category_Id":number  
+}
