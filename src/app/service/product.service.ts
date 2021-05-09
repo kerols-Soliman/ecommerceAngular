58f09628d63a2203e35514379875e73d79006810
kerols-Soliman <ksoliman998@gmail.com>
2021-05-09 03:32:07 +0200

    product service

---
+++ src/app/service/product.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { from, throwError } from 'rxjs';
+import { apiUrl } from 'src/config';
+import {HttpClient} from '@angular/common/http'
+import { IProduct } from '../interface/Product';
+import { catchError } from 'rxjs/operators'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+
+  constructor(private http:HttpClient) { }
+
+  url=apiUrl+"/api/Products";
+
+  GetById(id:number){
+    const newUrl=this.url+"/"+id;
+    return this.http.get<IProduct>(newUrl).pipe(catchError(err=>{
+      return throwError(err.message|| "There is an error");
+    }))
+  }
+
+
+}
